3826eaf693754803f5aa41517eda61f35f45e6b0
franksn90 <s.frankausowen@googlemail.com>
2021-12-21 12:04:12 +0100

    Rename players and make them final

--- src/main/java/de/unistuttgart/iste/sqa/pse/tiktaktoe/TicTacToeHamsterGame.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/tiktaktoe/TicTacToeHamsterGame.java
@@ -9,16 +9,16 @@ import de.hamstersimulator.objectsfirst.external.simple.game.SimpleHamsterGame;
  */
 public class TicTacToeHamsterGame extends SimpleHamsterGame {
 	private final Grid grid;
-	private TicTacToeHamster currentPlayer;
-	private TicTacToeHamster otherPlayer;
+	private final TicTacToeHamster crossPlayer;
+	private final TicTacToeHamster circlePlayer;
 
 	public TicTacToeHamsterGame() {
 		this.loadTerritoryFromResourceFile("/territories/territory.ter");
 		this.displayInNewGameWindow();
 		game.startGame();
 		this.grid = new Grid();
-		this.currentPlayer = turnIntoTicTacToeHamster(paule);
-		this.otherPlayer = createNewTicTacToeHamster();
+		this.crossPlayer = turnIntoTicTacToeHamster(paule);
+		this.circlePlayer = createNewTicTacToeHamster();
 		game.setSpeed(10.00); // Auto Speed Up the game
 	}
 
